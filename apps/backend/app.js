7295488466f1e55d56da561476c9acc01a7f44d5
Lamalice13 <139547486+Lamalice13@users.noreply.github.com>
2026-04-09 11:17:59 +0200

    feat: configuring cors

--- apps/backend/app.js
+++ apps/backend/app.js
@@ -1,13 +1,22 @@
 import express from "express";
 import "dotenv/config";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+const allowedOrigin = process.env.ALLOWED_ORIGIN.split(",") || [];
+
+const corsOptions = {
+  origin: allowedOrigin,
+  credentials: true,
+};
+app.use(cors(corsOptions));
+
 app.post("/login", (req, res) => {});
 
-app.listen(3000, (err) => {
+app.listen(process.env.PORT || 3000, (err) => {
   if (err) return console.error(err);
   console.log(`Server running on 3000`);
 });
