e30278ff969299ea459cbe09c3f563880ac61f6b
Lamalice13 <139547486+Lamalice13@users.noreply.github.com>
2026-04-13 15:15:22 +0200

    feat: set up prisma-pg adapter

--- apps/backend/lib/prisma.js
+++ apps/backend/lib/prisma.js
@@ -1,4 +1,9 @@
-import { PrismaClient } from "./generated/client";
-const prisma = new PrismaClient();
+import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaPg } from "@prisma/adapter-pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+
+const prisma = new PrismaClient({ adapter });
 
 export { prisma };
