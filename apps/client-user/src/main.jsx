c97bf48030ff68a2d2e04738ff6fbc776c5cbec0
Lamalice13 <139547486+Lamalice13@users.noreply.github.com>
2026-04-21 13:27:33 +0200

    chore(client-user): delete Nav component

--- apps/client-user/src/main.jsx
+++ apps/client-user/src/main.jsx
@@ -2,16 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App.jsx";
-import { Layout } from "./layout/Layout.jsx";
 import "@monorepo/shared/globals";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route element={<Layout />}>
-          <Route path='/' element={<App />} />
-        </Route>
+        <Route path='/' element={<App />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
